c3895e414a70ecefedd4cd09b7c3e7e881f258b0
jaksylwia <ceneemail@gmail.com>
2025-08-26 21:06:44 +0200

    naprawianie bledu z obrazami c.d.

--- game.js
+++ game.js
@@ -1,4 +1,6 @@
-
+/**
+ * @this {Phaser.Scene}
+ */
 const config = {
   type: Phaser.AUTO,
   width: 800,
@@ -16,11 +18,9 @@ const config = {
 
 let player;
 let cursors;
-let platforms;
-let obstacles;
 let score = 0;
 let scoreText;
-let gameOver = false;
+let obstacles;
 
 const game = new Phaser.Game(config);
 
@@ -28,12 +28,12 @@ function preload () {
   this.load.image('background', 'assets/sky4.png');
   this.load.image('ground', 'assets/platform.png');
   this.load.image('obstacle', 'assets/spike.png');
-  this.load.spritesheet('fox', 'assets/fox_sprite.png', { frameWidth: 32, frameHeight: 32 });
+  this.load.spritesheet('fox', 'assets/fox_all.png', { frameWidth: 32, frameHeight: 32 });
 }
 
 function create () {
   this.add.image(400, 300, 'background');
-  platforms = this.physics.add.staticGroup();
+  const platforms = this.physics.add.staticGroup();
   platforms.create(400, 584, 'ground').setScale(2).refreshBody();
 
   player = this.physics.add.sprite(100, 450, 'fox');
@@ -41,58 +41,71 @@ function create () {
   player.setCollideWorldBounds(true);
 
   this.anims.create({
-    key: 'run',
-    frames: this.anims.generateFrameNumbers('fox', { start: 0, end: 2 }),
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('fox', { start: 0, end: 1 }),
     frameRate: 10,
     repeat: -1
   });
 
-  player.anims.play('run', true);
+  this.anims.create({
+    key: 'turn',
+    frames: [ { key: 'fox', frame: 0 } ],
+    frameRate: 20
+  });
 
-  cursors = this.input.keyboard.createCursorKeys();
+  this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('fox', { start: 1, end: 2 }),
+    frameRate: 10,
+    repeat: -1
+  });
+
   this.physics.add.collider(player, platforms);
+  cursors = this.input.keyboard.createCursorKeys();
 
   obstacles = this.physics.add.group();
+  this.time.addEvent({
+    delay: 2000,
+    callback: addObstacle,
+    callbackScope: this,
+    loop: true
+  });
+
   this.physics.add.collider(obstacles, platforms);
   this.physics.add.overlap(player, obstacles, hitObstacle, null, this);
 
-  scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '24px', fill: '#000' });
-
-  this.time.addEvent({
-    delay: 2000,
-    callback: () => spawnObstacle(this),
-    loop: true
-  });
+  scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#000' });
 }
 
 function update () {
-  if (gameOver) return;
-
-  if (cursors.up.isDown && player.body.touching.down) {
-    player.setVelocityY(-350);
+  if (cursors.left.isDown) {
+    player.setVelocityX(-160);
+    player.anims.play('left', true);
+  } else if (cursors.right.isDown) {
+    player.setVelocityX(160);
+    player.anims.play('right', true);
+  } else {
+    player.setVelocityX(0);
+    player.anims.play('turn');
   }
 
-  obstacles.children.iterate(function (child) {
-    if (child) {
-      child.setVelocityX(-200);
-      if (child.x < -50) {
-        score += 10;
-        scoreText.setText('Score: ' + score);
-        child.destroy();
-      }
-    }
-  });
+  if (cursors.up.isDown && player.body.touching.down) {
+    player.setVelocityY(-330);
+  }
 }
 
-function spawnObstacle(scene) {
-  const obstacle = obstacles.create(850, 540, 'obstacle');
+function addObstacle() {
+  const obstacle = obstacles.create(800, 550, 'obstacle');
+  obstacle.setVelocityX(-200);
   obstacle.setCollideWorldBounds(false);
-  obstacle.setImmovable();
+  obstacle.setImmovable(true);
+  score += 10;
+  scoreText.setText('Score: ' + score);
 }
 
 function hitObstacle(player, obstacle) {
   this.physics.pause();
   player.setTint(0xff0000);
-  gameOver = true;
+  player.anims.play('turn');
   scoreText.setText('Game Over! Score: ' + score);
 }
